1965805652d614ab2a7b12dd215a15c79643e186
Greg Anderson <greg.1.anderson@greenknowe.org>
2020-09-20 16:27:32 -0700

    By default, multiplex module will redirect from path "/to/target" to "/target".

--- modules/custom/multiplex/src/Controller/MultiplexController.php
+++ modules/custom/multiplex/src/Controller/MultiplexController.php
@@ -3,6 +3,7 @@
 namespace Drupal\multiplex\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 
 /**
  * Returns responses for Multiplex routes.
@@ -12,25 +13,31 @@ class MultiplexController extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build() {
+  public function build($target) {
 
-    // Determine default target from potential locations.
-    $target = '/privacy';
-
-    // Redirect if target exists. It should always exist.
-    $destination = Url::fromUserInput($target);
-    if ($destination->isRouted()) {
-      return $this->redirect($destination->getRouteName());
+    if (!$target) {
+      $build['content'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('No target provided'),
+      ];
+      return $build;
     }
 
-    // Oh snap.
-    $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('Attempted to route to non-existant path %target', ['@target' => $target]),
-    ];
+    $url_object = \Drupal::service('path.validator')->getUrlIfValid("/$target");
 
-    return $build;
+    if (!$url_object) {
+      $build['content'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('Target %target does not exist', ['%target' => $target]),
+      ];
+      return $build;
+    }
 
+    $route_name = $url_object->getRouteName();
+    $route_parameters = $url_object->getrouteParameters();
+
+    // This sets the 'no-cache' header by default
+    return $this->redirect($route_name, $route_parameters);
   }
 
 }
