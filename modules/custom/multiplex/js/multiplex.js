cdf6f2d42f9b455eac164b8250d464b3e79eac8a
Kyle Adams <kyle@logic.fail>
2020-10-19 17:22:16 -0700

    hello world test

--- modules/custom/multiplex/js/multiplex.js
+++ modules/custom/multiplex/js/multiplex.js
@@ -1,17 +1,13 @@
-function Multiplex_static_content_dirs() {
-  $dir = drupal_get_path('module', 'Multiplex') . '/static_content';
-  return array(
-    $dir => array()
-  );
-}
-
 (function ($, Drupal) {
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
-    	objects = visitorCookieGetValue('STYXKEY_objects')
-      if (objects) {
-        $('body').append('<div style="background-color: orange; position: fixed; bottom: 0px;"><h1>Inventory: ' + objects + '</h1></div>')
-      }
+      //objects = visitorCookieGetValue('STYXKEY_objects')
+      //if (objects) {
+        //$('body').append('<div style="background-color: orange; position: fixed; bottom: 0px;"><h1>Inventory: ' + objects + '</h1></div>')
+      //}
+      let elem = document.createElement('DIV');
+      elem.innerHTML = "Hello world";
+      $('body').append(elem);
     }
   };
 })(jQuery, Drupal);
