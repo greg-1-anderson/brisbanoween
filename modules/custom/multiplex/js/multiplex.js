464910c754170a53e20c4e49108f80e88cb49294
Kyle Adams <kyle@logic.fail>
2020-10-19 17:08:59 -0700

    add static resources for inventory js extension

--- modules/custom/multiplex/js/multiplex.js
+++ modules/custom/multiplex/js/multiplex.js
@@ -1,9 +1,16 @@
+function Multiplex_static_content_dirs() {
+  $dir = drupal_get_path('module', 'Multiplex') . '/static_content';
+  return array(
+    $dir => array()
+  );
+}
+
 (function ($, Drupal) {
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
     	objects = visitorCookieGetValue('STYXKEY_objects')
       if (objects) {
-        $('body').append('<div style="background-color: orange; position: fixed; bottom: 0px;"><h1>Inventory: ' + objects + '</h1></div>')      	
+        $('body').append('<div style="background-color: orange; position: fixed; bottom: 0px;"><h1>Inventory: ' + objects + '</h1></div>')
       }
     }
   };
