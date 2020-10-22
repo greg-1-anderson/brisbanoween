2087861745203e0258acc720349ed9a2e8254c61
Kyle Adams <kyle@logic.fail>
2020-10-22 16:44:23 -0700

    maybe we cant do a compound date

--- modules/custom/multiplex/src/Form/SettingsForm.php
+++ modules/custom/multiplex/src/Form/SettingsForm.php
@@ -41,7 +41,7 @@ class SettingsForm extends ConfigFormBase {
 
 	$currentStartTime = $this->config('multiplex.settings')->get('game_start_time') ? DateTimePlus::createFromTimestamp(intval($this->config('multiplex.settings')->get('game_start_time'))) : DateTimePlus::createFromTimestamp(time());
     $form['game_start_time'] = [
-      '#type' => 'datetime',
+      '#type' => 'date',
       '#title' => $this->t('Game Start Time'),
       '#description' => $this->t("When does the game officially begin?"),
       '#default_value' => $currentStartTime
