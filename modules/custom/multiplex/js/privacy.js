0b873023e3dcb2460d4476774b64dfcf9e0c37e4
Kyle Adams <kyle@logic.fail>
2020-10-23 11:11:34 -0700

    ok, lets put it all back together again.

--- modules/custom/multiplex/js/privacy.js
+++ modules/custom/multiplex/js/privacy.js
@@ -1,10 +1,22 @@
-/*class PrivacyManager {
+class PrivacyManager {
 	constructor(cookieName, title, message, acceptButton, rejectButton) {
 		this.i_cookie_name = cookieName;
 		this.i_title_text = title;
 		this.i_message_text = message;
 		this.i_accept_button_text = acceptButton;
 		this.i_reject_button_text = rejectButton;
+
+		let cookies = this.getCookies();
+		this.i_first_open = true;
+		this.i_closed = !!cookies[this.i_cookie_name];
+
+		PrivacyManager.i_last_active = this;
+	}
+
+	static open() {
+		if (PrivacyManager.i_last_active != null) {
+			PrivacyManager.i_last_active.open();
+		}
 	}
 
 	getCookies() {
@@ -25,28 +37,42 @@
 		return cookies;
 	}
 
+	open() {
+		this.i_closed = false;
+		this.update();
+	}
+
 	accept() {
+		this.i_closed = true;
 		document.cookie = this.i_cookie_name + "=2; path=/";
 		this.update();
 	}
 
 	reject() {
+		this.i_closed = true;
 		document.cookie = this.i_cookie_name + "=0; path=/";
 		this.update();
 	}
 
 	update() {
 		if (this.i_element != null) {
-			let cookies = this.getCookies();
-			if (!cookies[this.i_cookie_name]) {
-				this.i_visible = true;
-				this.i_element.style.display = "";
-				this.i_real_wrapper.className = "PrivacyManager_wrapper PrivacyManager_wrapper_open";
+
+			if (this.i_closed !== true) {
+				if (this.i_visible != true) {
+					this.i_visible = true;
+					this.i_element.style.display = "";
+					this.i_real_wrapper.className = "PrivacyManager_wrapper" + (this.i_first_open != true ? " PrivacyManager_wrapper_open" : "");
+					this.i_notice.style.display = "none";
+					this.i_first_open = false;
+				}
 			}
 			else {
 				if (this.i_visible == true) {
 					this.i_visible = false;
 					this.i_real_wrapper.className = "PrivacyManager_wrapper PrivacyManager_wrapper_close";
+
+					let cookies = this.getCookies();
+					this.i_notice.style.display = cookies[this.i_cookie_name] == "2" ? "none" : "";
 				}
 			}
 		}
@@ -54,8 +80,22 @@
 
 	attach(component) {
 		if (this.i_element == null) {
+			let cookies = this.getCookies();
 			this.i_element = document.createElement('DIV');
 			this.i_element.className = "PrivacyManager";
+			this.i_element.style.display = !!cookies[this.i_cookie_name] ? "none" : "";
+
+				this.i_notice = document.createElement('DIV');
+				this.i_notice.className = "PrivacyManager_notice";
+				this.i_notice.style.display = cookies[this.i_cookie_name] == "0" ? "" : "none";
+				this.i_notice.title = "Privacy Settings";
+				this.i_notice.addEventListener("click", () => {
+					this.open();
+				});
+
+					this.i_notice_content = document.createElement('DIV');
+					this.i_notice_content.className = "PrivacyManager_notice_content";
+					this.i_notice.appendChild(this.i_notice_content);
 
 				this.i_real_wrapper = document.createElement('DIV');
 				this.i_real_wrapper.className = "PrivacyManager_wrapper";
@@ -93,14 +133,6 @@
 						this.i_button_wrapper.className = "PrivacyManager_button_wrapper";
 						this.i_centered_wrapper.appendChild(this.i_button_wrapper);
 
-							this.i_reject_button = document.createElement('DIV');
-							this.i_reject_button.className = "PrivacyMangaer_reject";
-							this.i_reject_button.innerHTML = this.i_reject_button_text;
-							this.i_reject_button.addEventListener("click", () => {
-								this.reject();
-							});
-							this.i_button_wrapper.appendChild(this.i_reject_button);
-
 							this.i_accept_button = document.createElement('DIV');
 							this.i_accept_button.className = "PrivacyMangaer_accept";
 							this.i_accept_button.innerHTML = this.i_accept_button_text;
@@ -110,10 +142,21 @@
 							this.i_button_wrapper.appendChild(this.i_accept_button);
 
 
+							this.i_reject_button = document.createElement('DIV');
+							this.i_reject_button.className = "PrivacyMangaer_reject";
+							this.i_reject_button.innerHTML = this.i_reject_button_text;
+							this.i_reject_button.addEventListener("click", () => {
+								this.reject();
+							});
+							this.i_button_wrapper.appendChild(this.i_reject_button);
+
+			this.update();
 		}
 		if (this.i_element.parentNode != null) {
+			this.i_notice.parentNode.removeChild(this.i_notice);
 			this.i_element.parentNode.removeChild(this.i_element);
 		}
+		component.appendChild(this.i_notice);
 		component.appendChild(this.i_element);
 	}
-}*/
+}
